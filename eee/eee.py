ac8de13d904e71dd83b4b983510dac5be060e146
skylineone044 <skylineone044@gmail.com>
2021-11-19 21:28:51 +0100

    TODO: fix assert

--- eee/eee.py
+++ eee/eee.py
@@ -6,6 +6,7 @@ import re
 
 LETTER = "e"
 PREPROCESSOR_DEFINES: dict = {}
+PREPROCESSOR_BLACKLIST = ("assert")
 
 
 def e_word(word: str) -> bool:
@@ -13,7 +14,7 @@ def e_word(word: str) -> bool:
 
 
 def generate_e(original: str) -> str:
-    if not e_word(original):
+    if not e_word(original) and original not in PREPROCESSOR_BLACKLIST:
         PREPROCESSOR_DEFINES[original] = (
             (len(PREPROCESSOR_DEFINES) + 1) * LETTER
             if original not in PREPROCESSOR_DEFINES.keys()
@@ -44,7 +45,10 @@ def generate_tokens(input: str, regex: str) -> None:
 
 def escape(token: str) -> str:
     return "".join(
-        [f"\\{letter}" if not letter.isalpha() and not letter.isnumeric() else letter for letter in list(token)]
+        [
+            f"\\{letter}" if not letter.isalpha() and not letter.isnumeric() else letter
+            for letter in list(token)
+        ]
     )
 
 
@@ -52,23 +56,24 @@ def convert(input: str, padding: str) -> str:
     tokens: list = list(PREPROCESSOR_DEFINES.keys())
     tokens.sort(reverse=True, key=len)
 
-    # regex_tokenMatcher = "[^A-Za-z0-9]{1}token[^A-Za-z0-9]{1}"
-
     res: str = ""
 
     for line in input.split("\n"):
         if not line.strip().startswith("#"):
             for token in tokens:
-            # regex = f"[^A-Za-z0-9]{escape(token)}[^A-Za-z0-9]"
-                line = line.replace(token, f"{padding}{PREPROCESSOR_DEFINES[token]}{padding}")
-                # line: str = re.sub(
-                #     regex,
-                #     f"{padding}{PREPROCESSOR_DEFINES[token]}{padding}",
-                #     line,
-                # )
+                line = line.replace(
+                    token, f"{padding}{PREPROCESSOR_DEFINES[token]}{padding}"
+                )
         res += line + "\n"
 
     return res
+
+
+def assert_handler(input: str) -> str:
+    for line in input.split("\n"):
+        if line.strip().startswith("assert"):
+            # line =
+            # fix assert
 
 
 def parse(inputFileName: str) -> str:
@@ -86,8 +91,7 @@ def parse(inputFileName: str) -> str:
         generate_tokens(file, regex_symbols)
         file = convert(file, padding=" ")
 
-
-    code: str = "".join(
+    code = "".join(
         [
             f"#define {e_word} {original}\n"
             for original, e_word in PREPROCESSOR_DEFINES.items()
