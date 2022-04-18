b9ee203194d3c27c5d4f37dc03cd704f75f85a57
skylineone044 <skylineone044@gmail.com>
2022-04-18 15:37:33 +0200

    setting in separate file

---
+++ moodle_bulk_assignment_dl/settings.py
@@ -0,0 +1,4 @@
+COOKIES = {"MoodleSession": "INSERT YOUR MOODLE COOKIE HERE"}
+DL_PAGE_ADDR = "INSERT YOUR MOODLE HUB TABLE PAGE ADDRESS HERE"  # for example: "https://moodle2.inf.u-szeged.hu/moodle38/mod/workshop/view.php?id=81" it works best if you set to see all entries in the moodle web interface, so the sript actually sees all the links, as it will not step through the pages for you
+DL_DIR = "/home/skyline/Downloads/bulk_moodle/cmd"  # set the directory for the results to be saved to
+DEFAULT_FILE_EXT = "zip"  # resutl file extenison
